b331c2e00a10f6ee92a0829d2ad4b98361f86000
Mason Albert <malber20@jhu.edu>
2021-09-12 13:29:20 -0400

    attempted new subtract implementation

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -237,7 +237,9 @@ Fixedpoint fixedpoint_sub(Fixedpoint left, Fixedpoint right) {
 
   if (right.frac > left.frac) {
     frac_diff = right.frac - left.frac;
-    frac_diff = (1UL << 63) - frac_diff;
+    // frac_diff = (1UL << 63) - frac_diff;
+    frac_diff = (~0UL) - frac_diff;
+    frac_diff++;
     whole_diff--;
   }
 
